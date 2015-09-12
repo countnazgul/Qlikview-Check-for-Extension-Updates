4fa559cca89fb3873413733081db844b60529fc9
Stefan Stoichev <stefan.stoichev@gmail.com>
2015-09-12 15:27:00 +0100

    Readme and optimization
    The script will check for update every time the session is started
    rather than on every selection/design change but the indicator (if new
    version is available) will stay

---
+++ README.md
@@ -0,0 +1,47 @@
+## Check for new versions of qlikview extensions
+
+There is no functionality in QV that will inform your users if new version of your QV extension is available. This script will help you do this.
+
+### Pre-requirements
+ - the extension code is hosted in Github
+ - need to create "Releases" for each new version
+
+### Setup releases in Github
+
+Please check the Github documentation how to create releases (https://help.github.com/articles/creating-releases/) 
+
+### Using the script
+
+Edit the "version" and "repoReleaseUrl" in the "checkforupdate.js"
+
+```
+		var version = 'x.xx';
+		var repoReleaseUrl = "https://api.github.com/repos/USERNAME/REPONAME/releases";
+```
+
+After this there are two changes that are needed to be made in the script of the actual extension:
+
+ - include the "checkforupdate.js" 
+ - call the "checkForUpdates" function
+ 
+### Include "checkforupdate.js"
+
+```
+var extension_path = Qva.Remote + "?public=only&name=Extensions/MyExtension/";
+Qva.LoadScript(extension_path + "checkforupdate.js", function() { ...
+```
+### Call "checkForUpdates" function
+Call the function after the creation of the main extension div while passing the newly created div id:
+```
+Qva.AddExtension('MyExtension', function(){
+  var _this = this;
+  var divName = _this.Layout.ObjectId.replace("\\", "_");
+  var ui = document.createElement("div");
+  ui.setAttribute("id", divName);
+  checkForUpdates(divName);
+  // ... your extension code as usual
+})
+```
+The script will check for new versions on every new session (not every time when selection is made) but the indicator will stay (if new version is available).
+
+The script is not dependant from any other scripts or libraries. 
--- checkforupdate.js
+++ checkforupdate.js
@@ -1,7 +1,18 @@
+var newExtensionVersionAvailable = false;
+var checkedForNewExtensionVersion = false;
+
 function checkForUpdates(divName) {
-try {
+	// Author: stefan.stoichev@gmail.com
+	// Repo: https://github.com/countnazgul/Qlikview-Check-for-Extension-Updates
+	// Info: If your QV extension is hosted in GitHub and there are releases configured
+	//       this script will check if there are new version available.
+	//       How to use it and how to setup releases and tags in Github? - check the readme file or 
+	//       the Github repo from the link above
+	
+	try {
 		var version = '0.1';
 		var repoReleaseUrl = "https://api.github.com/repos/countnazgul/RadialTree/releases";
+		var newVersionMessage = "New version of this extension is available!";
 		var xmlhttp;	
 		
 		var div = document.createElement("div");
@@ -12,11 +23,11 @@ try {
 			div.style.visibility = 'hidden';
 		
 		var arrowDiv = document.createElement("div");
-			arrowDiv.title = "New version of this extension is available!";
+			arrowDiv.title = newVersionMessage;
 			arrowDiv.id = "updateArrow";
 			arrowDiv.style.width = "0px";
 			arrowDiv.style.height = "0px";
-			arrowDiv.style.borderBottom = "7px solid green";
+			arrowDiv.style.borderBottom = "7px solid #00F010";
 			arrowDiv.style.borderLeft = "7px solid transparent";
 			arrowDiv.style.borderRight = "7px solid transparent";
 			
@@ -24,6 +35,10 @@ try {
 		
 		var a = document.getElementById(divName);
 			a.appendChild(div);	
+			
+		if(newExtensionVersionAvailable == true) {
+			document.getElementById('update').style.visibility = 'visible';
+		}
 		
 		if (window.XMLHttpRequest) {
 			xmlhttp = new XMLHttpRequest();
@@ -31,24 +46,25 @@ try {
 			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 		}
 
-		xmlhttp.onreadystatechange = function() {
-			if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
-			   if(xmlhttp.status == 200){
-					response = JSON.parse(xmlhttp.responseText);
-					
-					if( response[0].tag_name != version) {
-						document.getElementById('update').style.visibility = 'visible';
-					}			
-			   }
-			   else if(xmlhttp.status == 400) {
-			   }
-			   else {
-			   }
+		xmlhttp.onreadystatechange = function() {		
+			if(xmlhttp.status == 200){
+				response = JSON.parse(xmlhttp.responseText);
+									
+				if( response[0].tag_name != version) {
+					document.getElementById('update').style.visibility = 'visible';
+					newExtensionVersionAvailable = true;
+				}
+				
+				checkedForNewExtensionVersion = true;					
+			}			   
+			else if(xmlhttp.status == 400) {
 			}
+		}		
+		
+		if(checkedForNewExtensionVersion == false) {
+			xmlhttp.open("GET", repoReleaseUrl, true);
+			xmlhttp.send();
 		}
-
-		xmlhttp.open("GET", repoReleaseUrl, true);
-		xmlhttp.send();
 	} catch( err ) {
 	}
 }
